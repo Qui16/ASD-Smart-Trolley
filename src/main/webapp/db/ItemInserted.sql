5c7b19b00657b7937935604256afaba50730c033
Qui16 <quydam1609@gmail.com>
2021-09-09 19:55:07 +1000

    move sql file

---
+++ src/main/webapp/db/ItemInserted.sql
@@ -0,0 +1,16 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ * Author:  quyda
+ * Created: 09/09/2021
+ */
+insert into ITEMs("Item Name","Item Price","Item Quantity","Item ReceivedDate", "Item Region", "Item Description")
+values
+('Milk 1L',1.36,100,'2020-04-16','NSW','Fresh Milk'),
+('Milk 2L',2.36,100,'2020-04-16','NSW','Fresh Milk'),
+('Cookie Choc',7.54,100,'2020-04-16','QLD','Chocolate Cookies'),
+('Rosted Chic',10,100,'2020-04-16','NSW','Roasted Chicken'),
+('Sandwich',1.36,100,'2020-04-16','NSW','Sandwich');
