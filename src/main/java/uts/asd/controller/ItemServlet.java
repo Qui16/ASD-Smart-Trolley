7ee3a8effacc4abeba573df9c2adc0bf362350e0
Jack Hennessy <13553464@student.uts.edu.au>
2021-09-10 09:03:50 +1000

    adding item management files

---
+++ src/main/java/uts/asd/controller/ItemServlet.java
@@ -0,0 +1,35 @@
+package uts.asd.controller;
+
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import uts.asd.model.Customer;
+// import uts.asd.model.dao.DBManager;
+
+/**
+ *
+ * @author quyda
+ */
+public class ItemServlet extends HttpServlet {
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        // HttpSession session = request.getSession();
+        // String customer_email = request.getParameter("customer_email");
+        // String customer_name = request.getParameter("customer_name");
+        // String customer_password = request.getParameter("customer_password");
+
+        // session.setAttribute("customer_email", customer_email);
+        // session.setAttribute("customer_name", customer_name);
+        // session.setAttribute("customer_password", customer_password);
+        // request.getRequestDispatcher("CustomerAccount.jsp").forward(request,
+        // response);
+    }
+
+}
--- src/main/java/uts/asd/model/Product.java
+++ src/main/java/uts/asd/model/Product.java
@@ -14,85 +14,86 @@ public class Product {
 	private int id;
 	private String name;
 	private Double price;
-        private int quantity;
-        private String receivedDate;
-        private String region;
-        private String description;	
-	
+	private int quantity;
+	private String receivedDate;
+	private String region;
+	private String description;
+
 	public Product() {
 	}
-	
-	public Product(int id, String name, Double price, int quantity, String receivedDate, String region, String description) {
-            super();
-            this.id = id;
-            this.name = name;
-            this.price = price;
-            this.quantity = quantity;
-            this.receivedDate = receivedDate;
-            this.region = region;
-            this.description = description;
+
+	public Product(int id, String name, Double price, int quantity, String receivedDate, String region,
+			String description) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.price = price;
+		this.quantity = quantity;
+		this.receivedDate = receivedDate;
+		this.region = region;
+		this.description = description;
 	}
 
 	public int getId() {
-            return id;
+		return id;
 	}
 
 	public void setId(int id) {
-            this.id = id;
+		this.id = id;
 	}
 
 	public String getName() {
-            return name;
+		return name;
 	}
 
 	public void setName(String name) {
-            this.name = name;
-	}	
+		this.name = name;
+	}
 
 	public double getPrice() {
-            return price;
+		return price;
 	}
 
 	public void setPrice(Double price) {
-            this.price = price;
+		this.price = price;
 	}
 
-//        public int getQuantity() {
-//            return quantity;	
-//	}
-//
-//        public void setQuantity(int quantity) {
-//            this.quantity = quantity;
-//        }
-//
-//        public String date() {
-//            return receivedDate;
-//        }
-//
-//        public void setDate(String receivedDate) {
-//            this.receivedDate = receivedDate;
-//        }
-//
-//        public String region() {
-//            return region;
-//        }
-//
-//        public void setRegion(String region) {
-//            this region = region;
-//        }
-//
-//        public String description() {
-//            return description;
-//        }
-//
-//        public void setDesc(String description) {
-//            this description = description;
-//        }
+	// public int getQuantity() {
+	// return quantity;
+	// }
+	//
+	// public void setQuantity(int quantity) {
+	// this.quantity = quantity;
+	// }
+	//
+	// public String date() {
+	// return receivedDate;
+	// }
+	//
+	// public void setDate(String receivedDate) {
+	// this.receivedDate = receivedDate;
+	// }
+	//
+	// public String region() {
+	// return region;
+	// }
+	//
+	// public void setRegion(String region) {
+	// this region = region;
+	// }
+	//
+	// public String description() {
+	// return description;
+	// }
+	//
+	// public void setDesc(String description) {
+	// this description = description;
+	// }
 
 	@Override
 	public String toString() {
-		return "Product [id=" + id + ", name=" + name + ", price=" + price + ", quantity=" + quantity + ", received date=" + receivedDate + ", region=" + region + ", description=" + description;
+		return "Product [id=" + id + ", name=" + name + ", price=" + price + ", quantity=" + quantity
+				+ ", received date=" + receivedDate + ", region=" + region + ", description=" + description;
 	}
-	
-	
+
 }
