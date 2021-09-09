d53d425548381dd35a4a08fae2899d56897a7a6c
Qui16 <quydam1609@gmail.com>
2021-09-09 17:34:38 +1000

    java model

---
+++ src/main/java/uts/asd/controller/CustomerCreateServlet.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/*
+package uts.asd.controller;
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+// import uts.asd.model.dao.DBManager;
+
+/**
+ *
+ * @author quyda
+ */
+/*public class CustomerCreateServlet extends httpServlet {
+    
+    
+}
+*/
