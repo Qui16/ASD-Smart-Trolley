bae18377aab33e412c8110712016438ac7862f0c
Sern5011 <Casio4298@gmail.com>
2021-10-04 21:43:31 +1100

    PaymentServlet

--- src/main/java/uts/asd/controller/PaymentServlet.java
+++ src/main/java/uts/asd/controller/PaymentServlet.java
@@ -6,20 +6,22 @@
 package uts.asd.controller;
 
 import java.io.IOException;
-import java.io.PrintWriter;
 import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+import uts.asd.model.*;
+import uts.asd.model.dao.PaymentManager;
+import uts.asd.model.dao.DBConnector;
 
 /**
  *
  * @author casio
  */
-@WebServlet(name = "PaymentServlet", urlPatterns = {"/PaymentServlet"})
 public class PaymentServlet extends HttpServlet {
 
     @Override
@@ -28,6 +30,7 @@ public class PaymentServlet extends HttpServlet {
 
         // Session
         HttpSession session = request.getSession();
+        Validator validator = new Validator();
         String paymentMethod = request.getParameter("paymentMethod");
         String cardNumber = request.getParameter("cardNumber");
         String cvv = request.getParameter("cvv");
@@ -35,6 +38,38 @@ public class PaymentServlet extends HttpServlet {
         String expiryDate = request.getParameter("expiryDate");
         String datePaid = request.getParameter("datePaid");
 
-    }
+        Payment payment = new Payment(paymentMethod, cardNumber, expiryDate, cvv, nameOnCard, datePaid);
+        Customer user = (Customer) session.getAttribute("user");
+        PaymentManager manager = (PaymentManager) session.getAttribute("manager");
+        ScanCart orders = (ScanCart) session.getAttribute("order");
+        validator.clear(session);
 
+        try {
+
+            //payment = manager.searchPayment(payment_Id, datePaid);
+            //if (payment != null) {
+            //request.getRequestDispatcher("confirm_payment.jsp").include(request, response);
+            //} else {
+            //int order_Id = manager.getOrderId();
+            
+            //double orderPrice = orders.getTotalPrice();
+            int order_Id = (int)session.getAttribute("orderId");
+            //double orderPrice = manager.getPrice();
+            System.out.println(order_Id);
+            //System.out.println(orderPrice);
+            //session.setAttribute("INVOICE_ID", invoice_Id);
+            //session.setAttribute("PAY_ID", payment_Id);
+            //manager.addPayment(order_Id, paymentMethod, orderPrice, cardNumber, expiryDate, cvv, nameOnCard, datePaid);
+            int payment_Id = manager.getPaymentId(cardNumber);
+            payment = manager.searchPayment(payment_Id, datePaid);
+            //manager.addHistory(user.getCustomerID(), payment_Id, order_Id, paymentMethod, orderPrice, cardNumber, nameOnCard, datePaid);
+            session.setAttribute("payment", payment);
+
+            request.getRequestDispatcher("confirm_payment.jsp").include(request, response);
+            //}
+        } catch (SQLException | NullPointerException ex) {
+            request.getRequestDispatcher("create_payment.jsp").include(request, response);
+
+        }
+    }
 }
